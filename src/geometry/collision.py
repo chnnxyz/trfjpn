fb4c20cd41e79caf6067404ae5c9bb0966e58fe9
chnnxyz <s@chnn.xyz>
2025-10-17 15:00:34 +0900

    base map and mag collision

--- src/geometry/collision.py
+++ src/geometry/collision.py
@@ -4,37 +4,56 @@ from src.map.base_map import MapModel
 from src.map.mag import Mag
 
 
-def solve_left_collision(agent: Agent, x0: float):
+# Colissions against map
+def _solve_left_collision(agent: Agent, x0: float):
     if agent.bounds.x[0] <= x0:
         agent.state.x = 0
         agent.bounds.x = (0, 1)
 
 
-def solve_right_collision(agent: Agent, x1: float):
+def _solve_right_collision(agent: Agent, x1: float):
     if agent.bounds.x[1] >= x1:
         agent.state.x = x1 - 1.0
         agent.bounds.x = (x1 - 1.0, x1)
 
 
-def solve_top_collision(agent: Agent, y0: float):
+def _solve_top_collision(agent: Agent, y0: float):
     if agent.bounds.y[0] <= y0:
         agent.state.y = y0
         agent.bounds.y = (y0, y0 + 1.0)
 
 
-def solve_bottom_collision(agent: Agent, y1: float):
+def _solve_bottom_collision(agent: Agent, y1: float):
     if agent.bounds.y[1] >= y1:
         agent.state.y = y1 - 1.0
         agent.bounds.y = (y1 - 1.0, y1)
 
 
+def solve_mapbox_colissions(
+    agent: Agent, x0: float, x1: float, y0: float, y1: float
+) -> None:
+    _solve_left_collision(agent, x0)
+    _solve_right_collision(agent, x1)
+    _solve_top_collision(agent, y0)
+    _solve_bottom_collision(agent, y1)
+
+
+# Collisions against box elements
 def check_collision_with_mag(agent: Agent, mag: Mag) -> bool:
+    """Checks if an agent is adjacent to the mag, and returns true if it is.
+    This is used for action filtering.
+    """
     if (agent.bounds.x[1] >= mag.x1) and (
         mag.bounds.y[0] <= agent.bounds.y[1] <= mag.bounds.y[1]
         or mag.bounds.y[0] <= agent.bounds.y[0] <= mag.bounds.y[1]
     ):
         # Solve collision and push agent back
+        _solve_right_collision(agent, mag.x1)
 
         # allow for head entry action
         return True
+    return False
+
+
+def check_collision_with_lever(agent: Agent, lever: Lever) -> bool:
     return False
